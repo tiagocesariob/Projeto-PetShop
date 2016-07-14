9b0189e2ae3597d43006cfc1e135a5e07d3b1008
sergiocampos <sergio.campos@dce.ufpb.br>
2016-07-14 11:39:26 -0300

    Implementados Atributos e método na MainActivity para ler os componentes

--- PetShop/app/src/main/java/pdm/ifpb/edu/br/petshop/MainActivity.java
+++ PetShop/app/src/main/java/pdm/ifpb/edu/br/petshop/MainActivity.java
@@ -2,12 +2,21 @@ package pdm.ifpb.edu.br.petshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btcadastro, btlistar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.loadComponentes();
+    }
+
+    private void loadComponentes(){
+        this.btcadastro = (Button) findViewById(R.id.btcadastro);
+        this.btlistar = (Button) findViewById(R.id.btlistar);
     }
 }
